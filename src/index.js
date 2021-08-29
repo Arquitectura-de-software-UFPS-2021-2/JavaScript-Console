297d4b2e9d2c37a7491b7fef5a0e29eb2be0cce5
enmanuel <enmanuelcamilo9@hotmail.com>
2021-08-29 14:00:08 -0500

    Primera implementación para convertir documentos DOCX a ODT usando la API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer")
+const axios = require("axios")
+const fs = require("fs")
+const path = require("path")
 
 async function menu() {
+
+
 
     //Menú de las acciones que desea realizar el usuario
     const res_menu = await inquirer.prompt({
@@ -22,8 +27,72 @@ async function menu() {
 
     //Validar si el usuario desea salir del programa
     if (choice == 5) {
-        console.log("Hasta luego :)")
+        console.log("Hasta luego :)");
         return
     }
+
+    //Escribir la ruta del archivo
+    const ruta_archivo = await inquirer.prompt({
+        name: "ruta_archivo",
+        message: "Escribe la ruta del archivo"
+    })
+
+
+    // Codificar archivo a base64
+    const archivoBase64 = await base64_codificar(ruta_archivo.ruta_archivo);
+
+
+    //Petición a la API para convertir
+    let extensionDestino = ""
+    let extensionFuente = ""
+
+    let arrayRuta = ruta_archivo.ruta_archivo.split(path.sep)
+    let nombreArchivo = arrayRuta[arrayRuta.length - 1]
+
+    if (choice === 1) {
+        extensionDestino = "ODT"
+        extensionFuente = "DOCX"
+    } else if (choice === 2) {
+        extensionDestino = "ODS"
+        extensionFuente = "XLSX"
+    } else if (choice === 3) {
+        extensionDestino = "ODP"
+        extensionFuente = "PPTX"
+    }
+
+
+
+    const res = await axios.post('http://54.163.147.33:8080/convertir', {
+        "base64": archivoBase64,
+        "extencionDestino": extensionDestino,
+        "extencionFuente": extensionFuente,
+        "nombreArchivo": nombreArchivo
+    })
+
+    const data = res.data
+    base64_decodificar(data)
+
+
 }
 menu()
+
+function base64_codificar(path) {
+    let binaryData = fs.readFileSync(path) // leyendo datos binarios del archivo
+    let base64String = new Buffer(binaryData).toString("base64") //convirtiendo datos binarios a string en base 64
+    return base64String
+}
+
+
+
+
+function base64_decodificar(dataEnBase64) {
+    let stringBase64 = dataEnBase64.base64
+    let rutaArchivo = "../archivos_convertidos/" + dataEnBase64.nombreArchivo
+    const file = fs.writeFile(dataEnBase64.nombreArchivo, stringBase64, { enconding: "base64" }, function(err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("archivo creado")
+        }
+    })
+}
